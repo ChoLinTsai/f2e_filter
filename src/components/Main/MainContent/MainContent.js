5e3df35d250009d4962c7c39c236bf99edd3fcea
Cho Lin Tsai <max19851113@gmail.com>
2018-06-20 16:25:14 +0800

    fix format and add prettier ext.

--- src/components/Main/MainContent/MainContent.js
+++ src/components/Main/MainContent/MainContent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './mainContent.scss';
+import React, { Component } from "react";
+import "./mainContent.scss";
 
 export default class MainContent extends Component {
   constructor(props) {
@@ -8,133 +8,81 @@ export default class MainContent extends Component {
       stores: [
         {
           id: 1,
-          title: 'Kogi Cosby sweater.',
-          img: 'https://lorempixel.com/220/220/city/',
-          name: 'Ethan Foster',
-          detail: 'Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id.',
-          location: 'Kaohsiung City',
-          tags: 'Entertainment',
-          date: '2018/5/24 - 2018/5/31',
+          title: "Kogi Cosby sweater.",
+          img: "https://lorempixel.com/220/220/city/",
+          name: "Ethan Foster",
+          detail:
+            "Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id.",
+          location: "Kaohsiung City",
+          tags: "Entertainment",
+          date: "2018/5/24 - 2018/5/31"
         },
         {
           id: 2,
-          title: 'In the tumultuous business.',
-          img: 'https://lorempixel.com/220/220/city/',
-          name: 'Herman Cobb',
-          detail: 'It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.',
-          location: 'Kaohsiung City',
-          tags: 'Entertainment',
-          date: '2018/5/31 - 2018/6/14',
+          title: "In the tumultuous business.",
+          img: "https://lorempixel.com/220/220/city/",
+          name: "Herman Cobb",
+          detail:
+            "It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.",
+          location: "Kaohsiung City",
+          tags: "Entertainment",
+          date: "2018/5/31 - 2018/6/14"
         },
         {
           id: 3,
-          title: 'I have hinted that I would often...',
-          img: 'https://lorempixel.com/220/220/city/',
-          name: 'Landon Powers',
-          detail: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew.',
-          location: 'Kaohsiung City',
-          tags: 'Entertainment',
-          date: '2018/6/15 - 2018/6/30',
+          title: "I have hinted that I would often...",
+          img: "https://lorempixel.com/220/220/city/",
+          name: "Landon Powers",
+          detail:
+            "In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew.",
+          location: "Kaohsiung City",
+          tags: "Entertainment",
+          date: "2018/6/15 - 2018/6/30"
         }
       ]
     };
   }
   render() {
-
-    console.log(this.state.stores)
-    const mainContent = this.state.stores.map( i => {
-      return  <div key={i.id} className="col s12">
-                <div className="card horizontal">
-                  <div className="card-image">
-                    <img src={i.img} />
-                  </div>
-                  <div className="card-stacked">
-                    <div className="card-title">
-                      <h5>{i.title}</h5>
-                    </div>
-                    <div className="card-content">
-                      <p>{i.detail}</p>
-                    </div>
-                    <div className="card-action">
-                      <div className="nameTagBlock">
-                        <h6>{i.name}</h6>
-                        <div className="chip">
-                        {i.tags}
-                          <i className="close material-icons">close</i>
-                        </div>
-                      </div>
-
-                      <div className="locationDateBlock">
-
-                        <div className="locationIconEle">
-                          <i className="material-icons">location_on</i>
-                          <p>{i.location}</p>
-                        </div>
-                        
-                        <div className="dateIconEle">
-                          <i className="material-icons">date_range</i>
-                          <p>{i.date}</p>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-    })
-
-
-
-
-
-    return (
-      <section className="mainContentBlock">
-        {mainContent}
-        {/* <div className="col s12">
+    console.log(this.state.stores);
+    const mainContent = this.state.stores.map(i => {
+      return (
+        <div key={i.id} className="col s12">
           <div className="card horizontal">
-
             <div className="card-image">
-              <img src="https://lorempixel.com/220/220/city/" />
+              <img src={i.img} />
             </div>
-
             <div className="card-stacked">
               <div className="card-title">
-                <h5>Kogi Cosby sweater.</h5>
+                <h5>{i.title}</h5>
               </div>
-              
               <div className="card-content">
-                <p>Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id.</p>
+                <p>{i.detail}</p>
               </div>
-
               <div className="card-action">
                 <div className="nameTagBlock">
-                  <h6>Ethan Foster</h6>
+                  <h6>{i.name}</h6>
                   <div className="chip">
-                    Entertainment
+                    {i.tags}
                     <i className="close material-icons">close</i>
                   </div>
                 </div>
-
                 <div className="locationDateBlock">
-
                   <div className="locationIconEle">
                     <i className="material-icons">location_on</i>
-                    <p>Kaohsiung City</p>
+                    <p>{i.location}</p>
                   </div>
-                  
                   <div className="dateIconEle">
                     <i className="material-icons">date_range</i>
-                    <p>2018/5/24 - 2018/5/31</p>
+                    <p>{i.date}</p>
                   </div>
-
                 </div>
-
               </div>
             </div>
           </div>
-        </div> */}
-      </section>
-    )
+        </div>
+      );
+    });
+
+    return <section className="mainContentBlock">{mainContent}</section>;
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const isProd = process.env.NODE_ENV === 'production';
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'docs/')
+    path: path.resolve(__dirname, "docs/")
   },
   module: {
     rules: [
@@ -16,88 +16,86 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader'
+            loader: "babel-loader"
           }
         ]
       },
       {
         test: /\.scss$/,
-        use: isProd ? [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: './src/resources/*.scss'
-            }
-          }
-        ] : [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: './src/resources/*.scss'
-            }
-          }
-        ]
+        use: isProd
+          ? [
+              "style-loader",
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "postcss-loader",
+              "sass-loader",
+              {
+                loader: "sass-resources-loader",
+                options: {
+                  resources: "./src/resources/*.scss"
+                }
+              }
+            ]
+          : [
+              "style-loader",
+              "css-loader",
+              "postcss-loader",
+              "sass-loader",
+              {
+                loader: "sass-resources-loader",
+                options: {
+                  resources: "./src/resources/*.scss"
+                }
+              }
+            ]
       },
       {
         test: /\.(png|jpg|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              publicPath: './img/',
-              outputPath: 'img/',
+              name: "[name].[ext]",
+              publicPath: "./img/",
+              outputPath: "img/"
             }
           },
           {
-            loader: 'image-webpack-loader'
+            loader: "image-webpack-loader"
           }
         ]
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
         use: [
-          { loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							publicPath: './fonts/',
-							outputPath: 'fonts/'
-						}
-					}
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              publicPath: "./fonts/",
+              outputPath: "fonts/"
+            }
+          }
         ]
-      },
+      }
     ]
   },
   devServer: {
-    stats: "errors-only",
+    stats: "errors-only"
   },
   optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin({}),
-      new UglifyJsPlugin({}),
-    ]
+    minimizer: [new OptimizeCssAssetsPlugin({}), new UglifyJsPlugin({})]
   },
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
-      template: './src/index.html',
-      filename: 'index.html',
+      template: "./src/index.html",
+      filename: "index.html"
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.css',
-
-    }),
+      filename: "style.css"
+    })
   ]
-}
+};
 
-
-console.log(isProd ? 'It is Production' : 'It is Devolopment');
+console.log(isProd ? "It is Production" : "It is Devolopment");
